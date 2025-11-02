962da5fa9b7ae639f1820b00d1d8c2750c7409cb
BharatGawariya <108189519+BharatGawariya@users.noreply.github.com>
2025-11-02 18:58:10 +0530

    Remove unnecessary imports and whitespace

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -1,13 +1,11 @@
 # src/data_cleaning.py
-import pandas as pd
-
 def load_data(path):
     df = pd.read_csv(path, parse_dates=['date'])
     return df
-
 
 def basic_cleaning(df):
     df.columns = df.columns.str.strip().str.lower()
     df.dropna(subset=['sales'], inplace=True)
     df['date'] = pd.to_datetime(df['date'])
     return df
+
