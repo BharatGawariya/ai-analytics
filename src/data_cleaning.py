264fe771281aeb8eaebe2158c926b53091ca313c
BharatGawariya <108189519+BharatGawariya@users.noreply.github.com>
2025-11-02 18:52:27 +0530

    Update data_cleaning.py

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -5,6 +5,7 @@ def load_data(path):
     df = pd.read_csv(path, parse_dates=['date'])
     return df
 
+
 def basic_cleaning(df):
     df.columns = df.columns.str.strip().str.lower()
     df.dropna(subset=['sales'], inplace=True)
